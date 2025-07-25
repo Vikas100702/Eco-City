003a359c8eeeb484ddc2f624f2e49da99497e56c
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2025-07-25 08:21:07 +0530

    updating directory structure from bloc to cubit.

--- EcoCity_Admin/eco_city_admin/lib/main.dart
+++ EcoCity_Admin/eco_city_admin/lib/main.dart
@@ -1,13 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  debugPrint("Firebase initialized successfully");
   runApp(const MyApp());
 }
 
